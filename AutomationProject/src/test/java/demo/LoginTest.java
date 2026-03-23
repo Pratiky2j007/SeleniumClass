7011411626c9adb020080992d268aa183c1007bd
Pratiky2j007 <Pratik.y2j007@gmail.com>
2026-03-23 13:50:45 +0530

    allure reporting

--- AutomationProject/src/test/java/demo/LoginTest.java
+++ AutomationProject/src/test/java/demo/LoginTest.java
@@ -1,5 +1,6 @@
 package demo;
 
+import io.qameta.allure.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,18 +10,36 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@Feature("Login feature")
+@Epic("Login epic")
+@Story("Login verification")
 public class LoginTest {
+
+    WebDriver driver;
+    @TmsLink("123456")
     @Test
+    @Description("Verify login")
     public void loginTestMethod() throws MalformedURLException {
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.setCapability("platformName", "Windows");
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/"), chromeOptions);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/"), chromeOptions);
         //WebDriver driver=new ChromeDriver();
+        login();
+        close();
+    }
+
+    @Step("login step")
+    public void login(){
         driver.get("https://www.saucedemo.com/");
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+
+    }
+
+    @Step("close broswer step")
+    public void close(){
         driver.quit();
     }
 }
